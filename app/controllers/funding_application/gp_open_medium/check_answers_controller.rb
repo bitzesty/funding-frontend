e597d4cc42a66123c4b867a2fc445506beacc0a3
Stuart McColl <7103229+stuartmccoll@users.noreply.github.com>
2021-04-15 08:50:27 +0100

    Added check your answers to open medium grant journey
    This commit populate the 'Check your answers' page of the open medium grant programme journey.

--- app/controllers/funding_application/gp_open_medium/check_answers_controller.rb
+++ app/controllers/funding_application/gp_open_medium/check_answers_controller.rb
@@ -2,8 +2,7 @@
 class FundingApplication::GpOpenMedium::CheckAnswersController < ApplicationController
   include FundingApplicationContext
   include ObjectErrorsLogger
-  
-  # Stubbed method
+
   def update
 
     redirect_to :funding_application_gp_open_medium_confirm_declaration
