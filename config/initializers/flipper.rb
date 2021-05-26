f947b34ce29dc416c323e85136ad0c524728d584
Codespaces <codespaces@github.com>
2021-05-26 07:06:05 +0000

    Codespaces

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -1,22 +1,22 @@
 
 require 'flipper/adapters/active_record'
 
-if ActiveRecord::Base.connection.table_exists? :flipper_features
-  Flipper.configure do |config|
-    config.default do
-      adapter = Flipper::Adapters::ActiveRecord.new
-      Flipper.new(adapter)
-    end
-    # Flipper gates toggle app features on and off. Adding the flippers 
-    # here creates a row in flipper_features.
-    # To control toggles - see README
-    Flipper[:covid_banner_enabled].add
-    Flipper[:registration_enabled].add
-    Flipper[:new_applications_enabled].add
-    Flipper[:grant_programme_sff_small].add
-    Flipper[:project_enquiries_enabled].add
-    Flipper[:expressions_of_interest_enabled].add
-    Flipper[:payment_requests_enabled].add
-    Flipper[:grant_programme_sff_medium].add
-  end
-end
+# if ActiveRecord::Base.connection.table_exists? :flipper_features
+#   Flipper.configure do |config|
+#     config.default do
+#       adapter = Flipper::Adapters::ActiveRecord.new
+#       Flipper.new(adapter)
+#     end
+#     # Flipper gates toggle app features on and off. Adding the flippers 
+#     # here creates a row in flipper_features.
+#     # To control toggles - see README
+#     Flipper[:covid_banner_enabled].add
+#     Flipper[:registration_enabled].add
+#     Flipper[:new_applications_enabled].add
+#     Flipper[:grant_programme_sff_small].add
+#     Flipper[:project_enquiries_enabled].add
+#     Flipper[:expressions_of_interest_enabled].add
+#     Flipper[:payment_requests_enabled].add
+#     Flipper[:grant_programme_sff_medium].add
+#   end
+# end
