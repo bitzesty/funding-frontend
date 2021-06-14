1b5a2616d1050c315fce796a6b3dde935264799a
Paul Trelease <ptrelease@gmail.com>
2021-06-14 10:20:40 +0100

    removed reply_top param

--- app/mailers/notify_mailer.rb
+++ app/mailers/notify_mailer.rb
@@ -2,11 +2,9 @@ class NotifyMailer < Mail::Notify::Mailer
 
   include Devise::Controllers::UrlHelpers
 
-
   def confirmation_instructions(record, token, opts = {})
     template_mail('a44293b7-7263-42b4-8905-44bbedaf1dfa',
                   to: record.email,
-                  reply_to_id: Rails.configuration.x.reply_email_address,
                   personalisation: {
                       confirmation_url: confirmation_url(record, confirmation_token: token)
                   }
@@ -26,7 +24,6 @@ class NotifyMailer < Mail::Notify::Mailer
 
   def password_change(record, opts = {})
     template_mail('32bc1da4-99aa-4fde-9124-6c423cfcab15',
-                  reply_to_id: Rails.configuration.x.reply_email_address,
                   to: record.email
     )
   end
@@ -34,7 +31,6 @@ class NotifyMailer < Mail::Notify::Mailer
 
   def reset_password_instructions(record, token, opts = {})
     template_mail('343c89d0-0825-4363-a2eb-6a6bbc20a50f',
-                  reply_to_id: Rails.configuration.x.reply_email_address,
                   to: record.email,
                   personalisation: {
                       edit_password_url: edit_password_url(record, reset_password_token: token)
@@ -44,7 +40,6 @@ class NotifyMailer < Mail::Notify::Mailer
 
   def unlock_instructions(record, token, opts = {})
     template_mail('8c03a7c0-e23f-484e-9543-bbde9263bd47',
-                  reply_to_id: Rails.configuration.x.reply_email_address,
                   to: record.email,
                   personalisation: {
                       unlock_url: unlock_url(record, unlock_token: token)
@@ -56,7 +51,6 @@ class NotifyMailer < Mail::Notify::Mailer
   def project_submission_confirmation(project)
     template_mail('071cfcda-ebd4-4eba-8602-338b12edc4f9',
                   to: project.user.email,
-                  reply_to_id: Rails.configuration.x.reply_email_address,
                   personalisation: {
                       project_reference_number: project.funding_application.project_reference_number
                   }
@@ -67,7 +61,6 @@ class NotifyMailer < Mail::Notify::Mailer
   def project_enquiry_submission_confirmation(pre_application)
     template_mail('34ec207b-e8d1-46be-87ee-2eca4b665cbc',
                   to: pre_application.user.email,
-                  reply_to_id: Rails.configuration.x.reply_email_address,
                   personalisation: {
                       pa_project_enquiry_reference: pre_application.pa_project_enquiry.salesforce_pef_reference
                   }
@@ -78,7 +71,6 @@ class NotifyMailer < Mail::Notify::Mailer
   def expression_of_interest_submission_confirmation(pre_application)
     template_mail('76cba30c-e91b-4fae-bffc-78ee13179b9c',
                   to: pre_application.user.email,
-                  reply_to_id: Rails.configuration.x.reply_email_address,
                   personalisation: {
                       pa_expression_of_interest_reference: pre_application.pa_expression_of_interest.salesforce_eoi_reference
                   }
@@ -141,7 +133,6 @@ class NotifyMailer < Mail::Notify::Mailer
   def report_a_problem(message, name, email)
     template_mail("4d789cc6-bd6a-499f-bae2-502b633c098b",
                   to: Rails.configuration.x.support_email_address,
-                  reply_to_id: Rails.configuration.x.reply_email_address,
                   personalisation: {
                       message_body: message,
                       name: name,
@@ -152,7 +143,6 @@ class NotifyMailer < Mail::Notify::Mailer
   def question_or_feedback(message, name, email)
     template_mail("af4e775b-5a0e-4da2-81a1-3c51dd88a07c",
                   to: Rails.configuration.x.support_email_address,
-                  reply_to_id: Rails.configuration.x.reply_email_address,
                   personalisation: {
                       message_body: message,
                       name: name.present? ? name : "Name not provided",
