29f15e9025ee0a7678d4a3c455fe1a4b1be5021c
Paul Trelease <ptrelease@gmail.com>
2021-06-14 12:42:10 +0100

    reverted code to match the last working version - which was in commit ca143f1eddf869ab1711046e52c0a7e9a2d4f53f

--- app/mailers/notify_mailer.rb
+++ app/mailers/notify_mailer.rb
@@ -1,10 +1,14 @@
 class NotifyMailer < Mail::Notify::Mailer
 
+  before_action { @reply_to_id = "3bd255ca-7071-4cdb-aee8-f554bed5042d" }
+
   include Devise::Controllers::UrlHelpers
+
 
   def confirmation_instructions(record, token, opts = {})
     template_mail('a44293b7-7263-42b4-8905-44bbedaf1dfa',
                   to: record.email,
+                  reply_to_id: @reply_to_id,
                   personalisation: {
                       confirmation_url: confirmation_url(record, confirmation_token: token)
                   }
@@ -15,7 +19,7 @@ class NotifyMailer < Mail::Notify::Mailer
   def email_changed(record, opts = {})
     @resource = record
     view_mail('cd9fbf07-4960-4cb7-903c-068b76d2ca32',
-              reply_to_id: Rails.configuration.x.reply_email_address,
+              reply_to_id: @reply_to_id,
               to: @resource.email,
               subject: 'Email changed'
     )
@@ -24,6 +28,7 @@ class NotifyMailer < Mail::Notify::Mailer
 
   def password_change(record, opts = {})
     template_mail('32bc1da4-99aa-4fde-9124-6c423cfcab15',
+                  reply_to_id: @reply_to_id,
                   to: record.email
     )
   end
@@ -31,6 +36,7 @@ class NotifyMailer < Mail::Notify::Mailer
 
   def reset_password_instructions(record, token, opts = {})
     template_mail('343c89d0-0825-4363-a2eb-6a6bbc20a50f',
+                  reply_to_id: @reply_to_id,
                   to: record.email,
                   personalisation: {
                       edit_password_url: edit_password_url(record, reset_password_token: token)
@@ -40,6 +46,7 @@ class NotifyMailer < Mail::Notify::Mailer
 
   def unlock_instructions(record, token, opts = {})
     template_mail('8c03a7c0-e23f-484e-9543-bbde9263bd47',
+                  reply_to_id: @reply_to_id,
                   to: record.email,
                   personalisation: {
                       unlock_url: unlock_url(record, unlock_token: token)
@@ -51,6 +58,7 @@ class NotifyMailer < Mail::Notify::Mailer
   def project_submission_confirmation(project)
     template_mail('071cfcda-ebd4-4eba-8602-338b12edc4f9',
                   to: project.user.email,
+                  reply_to_id: @reply_to_id,
                   personalisation: {
                       project_reference_number: project.funding_application.project_reference_number
                   }
@@ -61,6 +69,7 @@ class NotifyMailer < Mail::Notify::Mailer
   def project_enquiry_submission_confirmation(pre_application)
     template_mail('34ec207b-e8d1-46be-87ee-2eca4b665cbc',
                   to: pre_application.user.email,
+                  reply_to_id: @reply_to_id,
                   personalisation: {
                       pa_project_enquiry_reference: pre_application.pa_project_enquiry.salesforce_pef_reference
                   }
@@ -71,6 +80,7 @@ class NotifyMailer < Mail::Notify::Mailer
   def expression_of_interest_submission_confirmation(pre_application)
     template_mail('76cba30c-e91b-4fae-bffc-78ee13179b9c',
                   to: pre_application.user.email,
+                  reply_to_id: @reply_to_id,
                   personalisation: {
                       pa_expression_of_interest_reference: pre_application.pa_expression_of_interest.salesforce_eoi_reference
                   }
@@ -84,7 +94,7 @@ class NotifyMailer < Mail::Notify::Mailer
     template_mail(
       'e35a0532-8b51-4447-bc6d-d39f705bd24c',
       to: funding_application.organisation.users.first.email,
-      reply_to_id: Rails.configuration.x.reply_email_address,
+      reply_to_id: @reply_to_id,
       personalisation: {
         project_reference_number: funding_application.project_reference_number,
         investment_manager_name: investment_manager_name,
@@ -118,7 +128,7 @@ class NotifyMailer < Mail::Notify::Mailer
     template_mail(
       'a990844f-700a-4185-9b77-6c1f33cf938e',
       to: recipient_email_address,
-      reply_to_id: Rails.configuration.x.reply_email_address,
+      reply_to_id: @reply_to_id,
       personalisation: {
         funding_application_id: funding_application_id,
         agreement_link: agreement_link,
@@ -133,6 +143,7 @@ class NotifyMailer < Mail::Notify::Mailer
   def report_a_problem(message, name, email)
     template_mail("4d789cc6-bd6a-499f-bae2-502b633c098b",
                   to: Rails.configuration.x.support_email_address,
+                  reply_to_id: @reply_to_id,
                   personalisation: {
                       message_body: message,
                       name: name,
@@ -143,6 +154,7 @@ class NotifyMailer < Mail::Notify::Mailer
   def question_or_feedback(message, name, email)
     template_mail("af4e775b-5a0e-4da2-81a1-3c51dd88a07c",
                   to: Rails.configuration.x.support_email_address,
+                  reply_to_id: @reply_to_id,
                   personalisation: {
                       message_body: message,
                       name: name.present? ? name : "Name not provided",
