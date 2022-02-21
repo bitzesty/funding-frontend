600e5a52296e4e37b5743e10e08c6e74e46603ab
Paul Trelease <ptrelease@gmail.com>
2022-02-21 08:43:59 +0000

    handled for applicant that isnt signed in (#917)

--- app/helpers/mailers/common_mailer_helper.rb
+++ app/helpers/mailers/common_mailer_helper.rb
@@ -1,4 +1,5 @@
 # Helper containing common code used by other mailer helpers.
+# User id will be logged as nil if applicant not signed in
 module Mailers::CommonMailerHelper
 
   def log_mails_sent(function_name)
@@ -14,20 +15,25 @@ module Mailers::CommonMailerHelper
 
   # Issues mocking current_user owing to Devise.  
   # These methods enable easy mocking
+  # Default is English.  Even if user not signed in.
   def send_english_mails?
-    current_user.send_english_mails?
+    if current_user.nil?
+      true
+    else
+      current_user.send_english_mails?
+    end
   end
 
   def send_welsh_mails?
-    current_user.send_welsh_mails?
+    current_user&.send_welsh_mails?
   end
 
   def send_bilingual_mails?
-    current_user.send_bilingual_mails?
+    current_user&.send_bilingual_mails?
   end
 
   def user_id
-    current_user.id
+    current_user&.id
   end
 
 end
