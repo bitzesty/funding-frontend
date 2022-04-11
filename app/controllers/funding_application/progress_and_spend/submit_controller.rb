1709cb6c378fc51c8c8231830a4ae43775a68ce5
Adam Lee <32270711+Gubbsy@users.noreply.github.com>
2022-04-11 11:38:35 +0100

    Feature/submit page (#959)
    * Add content to submission page based on tasks submitted (stubbed queries)
    
    * Fix line length

--- app/controllers/funding_application/progress_and_spend/submit_controller.rb
+++ app/controllers/funding_application/progress_and_spend/submit_controller.rb
@@ -1,12 +1,35 @@
 class FundingApplication::ProgressAndSpend::SubmitController < ApplicationController
   include FundingApplicationContext
+  before_action :authenticate_user!
   
     def show()
-  
+      initialise_view
     end
   
     def update()
   
+    end
+
+    private 
+
+    def initialise_view()
+      @user_email = current_user.email
+
+      @submitting_progress_update = @funding_application
+        .arrears_journey_tracker.progress_update.present?
+
+      # TODO: Pulll from JSON
+      @submitted_bank_details = false
+
+      # TODO: Pulll from JSON
+      @submitted_payment_request = false
+
+      # TODO: Pull down payment amount
+      @payment_amount = 1000
+
+      # TODO: Pull down payment percentage
+      @payment_percentage = 85
+
     end
   
   end
