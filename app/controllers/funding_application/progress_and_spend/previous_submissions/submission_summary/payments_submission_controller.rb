700b6ecc20c306ddbdb882c063d64448405d834c
Paul Trelease <ptrelease@gmail.com>
2022-05-30 09:51:18 +0100

    added high spend table to summary (#992)

--- app/controllers/funding_application/progress_and_spend/previous_submissions/submission_summary/payments_submission_controller.rb
+++ app/controllers/funding_application/progress_and_spend/previous_submissions/submission_summary/payments_submission_controller.rb
@@ -5,11 +5,38 @@ class FundingApplication::ProgressAndSpend::PreviousSubmissions::SubmissionSumma
   def show
 
     payment_request =
-    @funding_application.payment_requests.find(params[:payment_request_id])
+      @funding_application.payment_requests.find(params[:payment_request_id])
   
-    @low_spend = payment_request.low_spend
+    unless payment_request.low_spend.empty?
 
-    @update_total = @low_spend.sum {|ls| ls.total_amount + ls.vat_amount}
+      @low_spend = payment_request.low_spend
+
+      @low_spend_update_total =
+        @low_spend.sum {|ls| ls.total_amount + ls.vat_amount}
+
+      @low_spend_table_heading =
+        t(
+          "progress_and_spend.previous_submissions.submission_summary."\
+            "payments_summary.spend_under",
+          spend_amount: @low_spend.first.spend_threshold
+        )
+
+    end
+
+    unless payment_request.high_spend.empty?
+
+      @high_spend = payment_request.high_spend
+
+      @high_spend_update_total =
+        @high_spend.sum {|hs| hs.amount + hs.vat_amount}
+
+      @high_spend_table_heading =
+        t(
+          "progress_and_spend.previous_submissions.submission_summary."\
+            "payments_summary.spend_over",
+          spend_amount: @high_spend.first.spend_threshold
+        )
+    end
 
   end
 
