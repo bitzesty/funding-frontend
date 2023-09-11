f5caffe49e373a67c00ac1873247bece387225cb
JJD1990 <jack.douglas@rocketmail.com>
2023-09-11 14:14:52 +0000

    underscores added to investment_principles in investment_principles_controller.rb

--- app/controllers/pre_application/expression_of_interest/investment_principles_controller.rb
+++ app/controllers/pre_application/expression_of_interest/investment_principles_controller.rb
@@ -7,21 +7,21 @@ class PreApplication::ExpressionOfInterest::InvestmentPrinciplesController < App
   # re-rendering :show method if unsuccessful
   def update
 
-    logger.info 'Updating investment principles for ' \
+    logger.info 'Updating investment_principles for ' \
                 "pa_expression_of_interest ID: #{@pre_application.pa_expression_of_interest.id}"
 
     @pre_application.pa_expression_of_interest.validate_investment_principles = true
 
     if @pre_application.pa_expression_of_interest.update(pa_expression_of_interest_params)
 
-      logger.info 'Finished updating investment principles for pa_expression_of_interest ID: ' \
+      logger.info 'Finished updating investment_principles for pa_expression_of_interest ID: ' \
                   "#{@pre_application.pa_expression_of_interest.id}"
 
       redirect_to(:pre_application_expression_of_interest_heritage_focus)
 
     else
 
-      logger.info 'Validation failed when attempting to update investment principles ' \
+      logger.info 'Validation failed when attempting to update investment_principles ' \
                   " for pa_expression_of_interest ID: #{@pre_application.pa_expression_of_interest.id}"
 
       log_errors(@pre_application.pa_expression_of_interest)
