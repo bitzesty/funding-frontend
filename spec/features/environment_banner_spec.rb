1150bc0d20118d16937d00b4385639a68ef1af44
JJD1990 <jack.douglas@rocketmail.com>
2023-10-06 16:00:04 +0000

    adding env banner and relevant tests back into branch confirming pipeline does push to azure

--- spec/features/environment_banner_spec.rb
+++ spec/features/environment_banner_spec.rb
@@ -1,26 +1,26 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.feature 'test environment banner', type: :feature do
-#   scenario 'environment specific banner shown' do
-#     visit('/')
-#     expect(page.status_code).to eq(200)
-#     expect(page).to have_content('You are in environment: TEST')
-#   end
+RSpec.feature 'test environment banner', type: :feature do
+  scenario 'environment specific banner shown' do
+    visit('/')
+    expect(page.status_code).to eq(200)
+    expect(page).to have_content('You are in environment: TEST')
+  end
 
-#   scenario 'training environment banner shown' do
-#     ENV["RAILS_ENV"] = "training"
+  scenario 'training environment banner shown' do
+    ENV["RAILS_ENV"] = "training"
    
-#     visit('/')
-#     expect(page.status_code).to eq(200)
-#     expect(page).to have_content('You are in environment: TRAINING')
-#   end
+    visit('/')
+    expect(page.status_code).to eq(200)
+    expect(page).to have_content('You are in environment: TRAINING')
+  end
 
-#   scenario 'production environment banner not shown' do
-#     ENV["RAILS_ENV"] = "production"
+  scenario 'production environment banner not shown' do
+    ENV["RAILS_ENV"] = "production"
    
-#     visit('/')
-#     expect(page.status_code).to eq(200)
-#     expect(page).to have_no_content('You are in environment: PRODUCTION')
-#   end
+    visit('/')
+    expect(page.status_code).to eq(200)
+    expect(page).to have_no_content('You are in environment: PRODUCTION')
+  end
 
-# end
+end
